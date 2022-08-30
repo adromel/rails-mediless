619db14c04e8ec731dbb4ffaab96b8e15ea6eef5
florian badoil <fbadoil@hotmail.fr>
2022-08-30 16:33:06 +0200

    seed symptoms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'symptoms.csv'))
+
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  s = Symptom.new
+  s.name = row['name']
+  s.save
+  puts "#{t.name} saved"
+end
