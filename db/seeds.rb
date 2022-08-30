bb8ab80675f2059cc50e4950f3cbec544106e965
Alex <alexandre.dromel@gmail.com>
2022-08-30 16:34:17 +0200

    sseeeeeeeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'oils.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  t = EssentialOil.new
+  t.name = row['name']
+  t.description = row['description']
+  t.photo = row['photo']
+  t.save
+  puts "#{t.name} saved"
+end
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'users.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  u = User.new
+  u.email = row['email']
+  u.password = row['password']
+  u.save
+  puts "#{u.email} saved"
+end
