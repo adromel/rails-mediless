83191a8bb86d31cd82c4924fc7abff42c1845f60
Francois973 <robino.francois973@gmail.com>
2022-08-31 16:25:15 +0200

    oiltreatments controller ok :back-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
+  get 'oiltreatments/show'
   devise_for :users
   root to: "pages#home"
 
 
   # vue de la recherche symptoms
   resources :symptoms do
-    resources :essential_oils
-    resources :acu_points
+    resources :oil_treatments do
+      resources :essential_oils
+    end
   end
 
+
+    # resources :acu_points
+
+  # get '/symptoms/:id', to: 'symptoms#show'
   get "/test", to: "pages#test"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
