470ff0fbd99b956d0dad06d375e916f320126196
florian badoil <fbadoil@hotmail.fr>
2022-09-01 12:32:22 +0200

    erreur

---
+++ app/javascript/controllers/search_input_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search-input"
+export default class extends Controller {
+  static targets = ["input", "search"]
+
+  connect() {
+    console.log("------>")
+    // console.log(this.inputTarget)
+    // console.log(this.searchTarget)
+
+  }
+  research(event) {
+    console.log(event)
+        fetch(`/search?query=${this.inputTarget.value}`)
+    .then(response => response.text())
+    .then((htmlResult) => {
+      this.listTarget.innerHTML = htmlResult
+    })
+
+  }
+}
