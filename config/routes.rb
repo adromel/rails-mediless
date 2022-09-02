375c0fccf38ee4af77c5983e670cae3e1a9435fa
Antoine Gros <antoine.gros14@gmail.com>
2022-09-02 14:40:57 +0200

    saved element

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,20 @@ Rails.application.routes.draw do
   #vue de la recherche home
   get 'search', to: 'symptoms#search'
 
-
   # vue de la recherche symptoms
   resources :symptoms do
     resources :oil_treatments
   end
-  resources :essential_oils
+  resources :essential_oils do
+    resources :list_elements
+  end
 
   resources :symptoms do
     resources :acupoint_treatments
   end
-  resources :acu_points
-
-  resources :list_elements
+  resources :acupoints do
+    resources :list_elements
+  end
 
   # get '/symptoms/:id', to: 'symptoms#show'
   get "/test", to: "pages#test"
