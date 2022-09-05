5173b3becc11c4716f6b6305170c55a3d3d1af84
florian badoil <fbadoil@hotmail.fr>
2022-09-05 12:40:56 +0200

    saveindex

--- app/models/acupoint.rb
+++ app/models/acupoint.rb
@@ -2,4 +2,8 @@ class Acupoint < ApplicationRecord
   has_many :acupoint_treatments
   has_many :symptoms, through: :acupoint_treatments
   has_many :list_elements, as: :listable
+
+  def photo
+    nil
+  end
 end
