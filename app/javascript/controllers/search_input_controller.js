5d4f118da645a1705acb8dc491515b9bf7afa16a
Francois973 <robino.francois973@gmail.com>
2022-09-05 15:42:05 +0200

    fix problem with link navbar

--- app/javascript/controllers/search_input_controller.js
+++ app/javascript/controllers/search_input_controller.js
@@ -1,19 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search-input"
 export default class extends Controller {
-  static targets = ["input", "list"]
+  static targets = ["input", "list"];
 
   research() {
     fetch(`/search?query=${this.inputTarget.value}`)
-
-    .then(response => response.text())
-    .then((htmlResult) => {
-      this.listTarget.innerHTML = htmlResult
-    })
+      .then((response) => response.text())
+      .then((htmlResult) => {
+        this.listTarget.innerHTML = htmlResult;
+      });
   }
   connect() {
-    console.log("------>")
+    // console.log("------>")
     // console.log(this.inputTarget)
   }
 }
