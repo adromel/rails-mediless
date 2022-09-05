14af6ba6c5955444a58ad9ee3da4e4db810b6bfa
Antoine Gros <antoine.gros14@gmail.com>
2022-09-05 14:53:21 +0200

    geocoder at map

--- app/controllers/specialists_controller.rb
+++ app/controllers/specialists_controller.rb
@@ -1,7 +1,15 @@
 class SpecialistsController < ApplicationController
 
   def index
+    @specialists = Specialist.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @specialists.geocoded.map do |specialist|
+      {
+        lat: specialist.latitude,
+        lng: specialist.longitude
+      }
+    end
 
   end
-  
+
 end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import MapController from "./map_controller"
+application.register("map", MapController)
+
 import PosologyController from "./posology_controller"
 application.register("posology", PosologyController)
 
