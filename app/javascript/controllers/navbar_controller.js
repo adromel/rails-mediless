864f36aa1f3db835152fff8e64bb31c0975be4fd
Francois973 <robino.francois973@gmail.com>
2022-09-05 14:46:36 +0200

    fix format mobile

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,23 +4,20 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["list", "indicator"];
 
-  connect() {
-    console.log("----->");
-  }
+  // connect() {
+  //   console.log("----->");
+  // }
 
-  update(event) {
-    event.preventDefault();
-    this.listTargets.forEach((list) => {
-      list.classList.remove("active");
-    });
-    event.currentTarget.classList.add("active");
+  // update(event) {
+  //   event.preventDefault();
+  //   this.listTargets.forEach((list) => {
+  //     list.classList.remove("active");
+  //   });
+  //   event.currentTarget.classList.add("active");
 
-    console.log(this.listTargets.indexOf(event.target.parentNode));
-
-    this.indicatorTarget.style.left = `${
-      this.listTargets.indexOf(event.target.parentNode) * 69
-    }px`;
-
-    // nav_indicator.style.left = `calc(${i * 120 + 10}px - 158px)`;
-  }
+  //   this.indicatorTarget.style.left = `${
+  //     this.listTargets.indexOf(event.target.parentNode) * 69
+  //   }px`;
+  //   // nav_indicator.style.left = `calc(${i * 120 + 10}px - 158px)`;
+  // }
 }
