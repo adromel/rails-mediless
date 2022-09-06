6b75833d4cbcefeae27fc8110795f60bb5bb09f3
florian badoil <fbadoil@hotmail.fr>
2022-09-06 14:20:10 +0200

    détails

--- app/helpers/list_element_helper.rb
+++ app/helpers/list_element_helper.rb
@@ -1,5 +1,8 @@
 module ListElementHelper
 
+#helper pour la view index de saving list
+#afin d'afficher les symptoms liés aux points d'acu
+
   def display_acupoint_of(list_element)
     list_element.listable.symptoms.map(&:name).join(" ")
   end
