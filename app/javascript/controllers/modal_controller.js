8badc694f8cc6bbcbe775dee41ad782cadb87ddc
Francois973 <robino.francois973@gmail.com>
2022-09-07 14:20:11 +0200

    Fixes

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,18 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="popup-to-search"
 export default class extends Controller {
   static targets = ["opener", "closer"];
 
-  connect() {
-    console.log("Toto");
-  }
+  connect() {}
 
   open() {
-    this.openerTarget.classList.remove("hidden")
+    console.log("Click");
+    this.openerTarget.classList.remove("hidden");
   }
 
   close() {
-    this.closerTarget.classList.add("hidden")
+    this.closerTarget.classList.add("hidden");
   }
 }
