b47abc7854a08e7b42f97b62b6bdc597bdb00d3e
Alex <alexandre.dromel@gmail.com>
2022-09-08 12:07:42 +0200

    fix

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -74,7 +74,8 @@ export default class extends Controller {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.maxDistance = 3;
     this.controls.minDistance = 0.2;
-    this.controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
+    // this.controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
+    this.controls.enablePan = false;
 
     //SPHERE
 
